d98c49895cb9feefa6e722ff01caa0a2f3e97ec4
Steve Vovchyna <stevenvovchyna@Steve.local>
2019-05-31 11:36:33 +0300

    whatev

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 /* Main page with two forms: sign up and log in */
 require 'db.php';
+include 'indexito.php';
 session_start();
 ?>
 <!DOCTYPE html>
@@ -10,19 +11,6 @@ session_start();
 	<title>Sign-Up/Login Form</title>
 	<?php include 'css/css.html'; ?>
 </head>
-
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-    if (isset($_POST['login'])) { //user logging in
-        require 'login.php';
-    }
-    elseif (isset($_POST['register'])) { //user registering   
-        require 'register.php';
-    }
-}
-?>
-
 <body>
 	<div class="form">
 
